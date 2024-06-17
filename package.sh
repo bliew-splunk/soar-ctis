b6d498d706612ae956d3f3d3b2f113ea3ce16ed0
Ben Liew <bliew@splunk.com>
2024-06-17 14:56:39 +1000

    update package script

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+set -xe
 version=$(jq -r ".app_version" ctis.json)
 output="build/ctis-$version.tgz"
 echo "Output: $output"
-tar --exclude=".*" --exclude="build" -C /Users/bliew/dev -czvf "$output" soar-ctis
+tar --exclude=".*" --exclude="build" -C ../ -czvf "$output" soar-ctis
